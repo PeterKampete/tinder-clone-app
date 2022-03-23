7b758565f3dd7b4c613799e82958d1f406df99e6
PeterKampete <peterkampete01@gmail.com>
2022-03-23 21:09:08 +0100

    working on home page

--- src/Header.js
+++ src/Header.js
@@ -2,23 +2,32 @@ import React from "react";
 import { IconButton } from "@material-ui/core";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
+import Search from "@material-ui/icons/Search"
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <IconButton>
-        <PersonIcon fontSize="large" className="header_icon" />
-      </IconButton>
+      <div className="header_logo_container">
+        <img
+          className="header_logo"
+          src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
+          alt=""
+        />
+      </div>
+      <div className="input_container">
+        <input className="input" placeholder="Search a date" />
+        <Search className="search_icon" color="disabled" />
+      </div>
+      <div className="header_icon_container">
+        <IconButton>
+          <PersonIcon fontSize="large" className="header_icon" />
+        </IconButton>
 
-      <img
-        className="header_logo"
-        src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
-        alt=""
-      />
-      <IconButton>
-        <ForumIcon fontSize="large" className="header_icon" />
-      </IconButton>
+        <IconButton>
+          <ForumIcon fontSize="large" className="header_icon" />
+        </IconButton>
+      </div>
     </div>
   );
 }
--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -8,22 +8,24 @@ import FlashOnIcon from "@material-ui/icons/FlashOn";
 import { IconButton } from "@material-ui/core";
 function SwipeButtons() {
   return (
-    <div className="swipeButtons">
-      <IconButton className="swipeButtons__repeat">
-        <ReplayIcon fontSize="large" />
-      </IconButton>
-      <IconButton className="swipeButtons__left">
-        <CloseIcon fontSize="large" />
-      </IconButton>
-      <IconButton className="swipeButtons__star">
-        <StarRateIcon fontSize="large" />
-      </IconButton>
-      <IconButton className="swipeButtons__right">
-        <FavoriteIcon fontSize="large" />
-      </IconButton>
-      <IconButton className="swipeButtons__lightning">
-        <FlashOnIcon fontSize="large" />
-      </IconButton>
+    <div className="container">
+      <div className="swipeButtons">
+        <IconButton className="swipeButtons__repeat">
+          <ReplayIcon fontSize="medium" />
+        </IconButton>
+        <IconButton className="swipeButtons__left">
+          <CloseIcon fontSize="medium" />
+        </IconButton>
+        <IconButton className="swipeButtons__star">
+          <StarRateIcon fontSize="medium" />
+        </IconButton>
+        <IconButton className="swipeButtons__right">
+          <FavoriteIcon fontSize="medium" />
+        </IconButton>
+        <IconButton className="swipeButtons__lightning">
+          <FlashOnIcon fontSize="medium" />
+        </IconButton>
+      </div>
     </div>
   );
 }
